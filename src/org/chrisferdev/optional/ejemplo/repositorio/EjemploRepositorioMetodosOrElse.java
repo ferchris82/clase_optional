370e998ae19a5989e9e5c69609ede98b2be521d4
Christian <ferchris82@hotmail.com>
2024-01-11 13:27:44 -0500

    Versión 0.1

---
+++ src/org/chrisferdev/optional/ejemplo/repositorio/EjemploRepositorioMetodosOrElse.java
@@ -0,0 +1,23 @@
+package org.chrisferdev.optional.ejemplo.repositorio;
+
+import org.chrisferdev.optional.ejemplo.models.Computador;
+
+public class EjemploRepositorioMetodosOrElse {
+    public static void main(String[] args) {
+
+        Repositorio<Computador> repositorio = new ComputadorRepositorio();
+
+        Computador defecto = new Computador("HP Omen", "LA0001");
+
+        Computador pc = repositorio.filtrar("rog").orElse(valorDefecto());
+        System.out.println(pc);
+
+        pc = repositorio.filtrar("macbook pro").orElseGet(EjemploRepositorioMetodosOrElse::valorDefecto);
+        System.out.println(pc);
+    }
+
+    public static Computador valorDefecto(){
+        System.out.println("Obteniendo el valor por defecto!!!");
+        return new Computador("HP Omen", "LA0001");
+    }
+}
